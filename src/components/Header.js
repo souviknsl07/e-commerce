a243669e57ab5de88e46699c0a81aa340a55745f
Souvik Nath <souviknsl07@gmail.com>
2021-08-05 17:49:37 +0530

    remove permission file

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
-import { selectGroupedItems, selectItems } from "../slices/basketSlice";
+import { selectItems } from "../slices/basketSlice";
 
 const Header = ({ search, setSearch }) => {
   const [session] = useSession();
